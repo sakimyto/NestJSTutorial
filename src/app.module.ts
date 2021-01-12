bd26d3eb9a315ff92745bf8a4e586ba7c0315ea0
sakimyto <21049958+sakimyto@users.noreply.github.com>
2021-01-12 12:44:17 +0900

    Add dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ItemModule],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
